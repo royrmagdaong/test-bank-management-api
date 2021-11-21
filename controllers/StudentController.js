d8e01ab89cf9e20c003edd39f92fc7dc110c77e3
unknown <royrmagdaong@gmail.com>
2021-11-21 22:45:44 +0800

    delete department model, add pagination, and search field for student

--- controllers/StudentController.js
+++ controllers/StudentController.js
@@ -4,26 +4,48 @@ const Student = require('../models/student')
 module.exports = {
     getStudents: async (req, res) => {
         try {
-            await Student.aggregate([
-                {
-                  $addFields: {
-                    full_name: {
-                      $concat: ["$first_name", ' ',"$middle_name", ' ',"$last_name"],
-                    }
-                  },
+          let searchString = req.body.searchString
+          let limit = req.body.limit
+          let skip = req.body.skip
+          let regexp = new RegExp("^"+ searchString, 'i')
+
+          const count = await Student.countDocuments({ $or: [
+            {student_id: regexp}, 
+            {first_name: regexp}, 
+            {middle_name: regexp}, 
+            {last_name: regexp}] 
+          });
+
+          await Student.aggregate([
+              {
+                $addFields: {
+                  full_name: {
+                    $concat: ["$first_name", ' ',"$middle_name", ' ',"$last_name"],
+                  }
                 },
-                {
-                  $match: {
-                    full_name: {
-                      $regex: '',
-                      $options: "i",
-                    }
-                  },
-                },
-              ]).exec((error, students) => {
-                if(error) return res.json({response: false, message: error.message})
-                return res.json({response: true, data: students})
-            })
+              },
+              {
+                $match: {
+                  $or: [
+                    {student_id:regexp},
+                    {first_name:regexp},
+                    {middle_name:regexp},
+                    {last_name:regexp},
+                  ]
+                }
+              },
+              { $limit: limit + skip },
+              { $skip: skip }
+            ]).exec((error, students) => {
+              if(error) return res.json({response: false, message: error.message})
+              return res.json({
+                response: true, 
+                data: students, 
+                count: count,
+                limit: limit,
+                skip: skip
+              })
+          })
         } catch (error) {
             return res.json({response: false, message: error.message})
         }
